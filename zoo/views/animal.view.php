5c03ca35f1ee51c5cad0cb4501b17c7115e99fc4
Muccachino <lucas_staszewski@yahoo.de>
2024-05-15 11:38:15 +0200

    add info page for each animal

--- zoo/views/animal.view.php
+++ zoo/views/animal.view.php
@@ -17,7 +17,33 @@ if (isset($_GET["name"]) && isset($theZoo)) {
     <main style="padding: 0">
         <h2><?= $animal->getName() ?></h2>
         <p>Sex: <?= $animal->getSex() ?></p>
-        <p>Current Weight: <?= $animal->getWeight() ?> kg</p>
+
+        <br>
+      <?php
+      if ($animal instanceof BigCat):?>
+          <p>How Dangerous is <?= $animal->getName() ?> ? (0-5)
+              <progress value="<?= $animal->getDangerLevel() ?>" max="5"></progress>
+          </p>
+          <p><?php $animal->roar(); ?></p>
+      <?php endif; ?>
+      <?php
+      if (!($animal instanceof BigCat)): ?>
+          <p>No Danger Parameter for this kind of Animal</p>
+          <p><?php $animal->pet() ?></p>
+      <?php endif; ?>
+        <br>
+        <div>
+            <p>Min. Weight: (<?= $animal->getMinWeight() ?> kg)
+                <progress value="<?= $animal->getMinWeight() ?>" max="<?= $animal->getMaxWeight() ?>"></progress>
+            </p>
+            <p>Current Weight: (<?= $animal->getWeight() ?> kg)
+                <progress value="<?= $animal->getWeight() ?>" max="<?= $animal->getMaxWeight() ?>"></progress>
+            </p>
+            <p>Max. Weight: (<?= $animal->getMaxWeight() ?> kg)
+                <progress value="<?= $animal->getMaxWeight() ?>" max="<?= $animal->getMaxWeight() ?>"></progress>
+            </p>
+        </div>
+
         <a href="../index.php"><-- Back to the Zoo</a>
     </main>
 
